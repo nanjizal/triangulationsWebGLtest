e800804e7faee600335864ee394299b0ed9da38c
nanjizal <nitsuj@nanjizal.net>
2017-04-28 16:32:44 +0100

    Create TestEnclosingTriangle.hx

---
+++ triangulationsWebGLtest/visualTests/TestEnclosingTriangle.hx
@@ -0,0 +1,15 @@
+package triangulationsWebGLtest.visualTests;
+import triangulations.FillShape;
+import justTriangles.PathContext;
+import triangulationsWebGLtest.helpers.Draw;
+import triangulations.Geom2;
+class TestEnclosingTriangle {
+    public static inline 
+    function draw( shape: FillShape, draw: Draw ){
+        /// NOT yet implemented externally requires more thought;
+        // geom
+        var ctx: PathContext;
+        // render
+        return ctx;
+    }
+}
