7fc4dcf4449ec87d4e42a8d396e4388315cb3624
nanjizal <nitsuj@nanjizal.net>
2017-05-05 15:50:14 +0100

    update to allow interactive flipping of quad mesh

--- triangulationsWebGLtest/visualTests/TestQuadEdge.hx
+++ triangulationsWebGLtest/visualTests/TestQuadEdge.hx
@@ -9,7 +9,7 @@ import triangulationsWebGLtest.helpers.Draw;
 import triangulations.Geom2;
 class TestQuadEdge {
     public static inline 
-    function draw( shape: FillShape, draw: Draw ){
+    function draw( shape: FillShape, draw: Draw, toggle: Bool ){
         // geom
         var vert = shape.vertices;
         var face = shape.faces;
@@ -18,23 +18,30 @@ class TestQuadEdge {
         var sideEdges = new Array<SideEdge>();
         Triangulate.makeQuadEdge( vert, edges, coEdges, sideEdges );
         // render
-        //edges.flipEdge( coEdges, sideEdges, 12 );
+        draw.sevenSegOnEdges = true;
         var ctx = new PathContext( 1, 1024, 0, 0 );
         draw.titleTextBlue( 'Quad Edge test', ctx );
+        draw.textViolet( 'click on yellow square to flip', ctx, 10, 1000 );
         var thick = 4;
         ctx.setThickness( 4 );
         ctx.fill = true;
-        ctx.setColor( 0, 3 );
+        ctx.setColor( 2, 3 );
         ctx.moveTo( 0, 0 );
-        draw.edges( edges, shape, ctx, true );
+        if( !toggle )  draw.edges( edges, shape, ctx, true );
         ctx.fill = true;
-        ctx.setColor( 5, 2 );
+        ctx.setColor( 7, 2 );
         ctx.moveTo( 0, 0 );
-        // geom
-        edges.flipEdge( coEdges, sideEdges, 12 );
-        // render
-        draw.edges( edges, shape, ctx, true );
+        if( toggle ) {
+            edges.flipEdge( coEdges, sideEdges, 12 );
+            draw.edges( edges, shape, ctx, true );
+        }
+        ctx.setColor( 3, 3 );
+        ctx.moveTo( 0, 0 );
+        // yellow square on edge 12.
+        var e = edges[12];
+        draw.squareBetween( 2, ctx, vert[e.p], vert[e.q] );
         ctx.render( thick, false );
+        draw.sevenSegOnEdges = false;
         return ctx;
     }
 }
